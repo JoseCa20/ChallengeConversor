14fcb31340646ee5d5e049e6f51870c76e10f099
joseca20 <crisskcres1720@gmail.com>
2024-10-17 07:12:02 -0500

    Se realizan algunas modificaciones para eliminar swith y se deja funcional la app de conversion de moneda

--- src/com/jeictechnology/conversormoneda/principal/Main.java
+++ src/com/jeictechnology/conversormoneda/principal/Main.java
@@ -4,11 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
 import com.jeictechnology.conversormoneda.modelos.Moneda;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -35,80 +33,91 @@ public class Main {
 
         List<String> lista = new ArrayList<>(monedas.keySet());
 
-        System.out.println("*********************************************************************************************");
-        System.out.println("Bienvenid@, conversor de moneda, por favor selecciona el número de la moneda que desea convertir"+"\n");
+        boolean continuar = true;
 
-        final int[] contador = {1};
-        monedas.forEach((pais, mon) -> {
-            System.out.println(contador[0] + " "+pais + " "+mon.getCodigo());
-            contador[0]++;
-        });
+        while (continuar){
+            System.out.println("*********************************************************************************************");
+            System.out.println("Bienvenid@ al conversor de moneda, por favor selecciona el número de la moneda que desea convertir"+"\n");
 
-        int opcion = scanner.nextInt();
+            final int[] contador = {1};
+            monedas.forEach((pais, mon) -> {
+                System.out.println(contador[0] + " "+pais + " "+mon.getCodigo());
+                contador[0]++;
+            });
 
-        System.out.println("-----------------------------------------------");
-        System.out.println("Ingresa la cantidad a convertir: "+"\n");
+            System.out.println(contador[0] + " Salir");
 
-        double cantidadAConvertir = scanner.nextDouble();
+            int opcion = scanner.nextInt();
 
-        System.out.println("-----------------------------------------------");
-        System.out.println("Selecciona la moneda de conversión"+"\n");
-
-        final int[] contadorConvertidor = {1};
-        monedas.forEach((pais, mon) -> {
-            System.out.println(contadorConvertidor[0] + " "+pais + " "+mon.getCodigo());
-            contadorConvertidor[0]++;
-        });
-
-        int opcionConvertir = scanner.nextInt();
-
-        String paisSeleccionado = lista.get(opcion - 1);
-        String paisSeleccionadoAConvertir = lista.get(opcionConvertir - 1);
-
-        String url = "https://v6.exchangerate-api.com/v6/";
-        String apiKey = "ad70ff102a291a8321ed3820";
-        String complemento = "/latest/"+monedas.get(paisSeleccionado).getCodigo();
-
-
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(url+apiKey+complemento)).build();
-        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-
-        String json = httpResponse.body();
-
-        //System.out.println(json);
-
-        Gson gson = new Gson();
-        JsonElement jsonElement = JsonParser.parseString(json);
-        JsonObject jsonObject = jsonElement.getAsJsonObject();
-
-        JsonObject conversionRates = jsonObject.getAsJsonObject("conversion_rates");
-
-        List<String> listaCodigos = new ArrayList<>();
-        conversionRates.keySet().forEach(listaCodigos::add);
-
-        //System.out.println(lista);
-        //System.out.println(monedas.get(paisSeleccionado).getCodigo());
-        //System.out.println(monedas.get(paisSeleccionadoAConvertir).getCodigo());
-        //System.out.println(listaCodigos);
-        //System.out.println(conversionRates);
-        System.out.println(listaCodigos.contains(monedas.get(paisSeleccionadoAConvertir).getCodigo()));
-        System.out.println(conversionRates.get(monedas.get(paisSeleccionadoAConvertir).getCodigo()).getAsDouble());
-        System.out.println((conversionRates.get(monedas.get(paisSeleccionadoAConvertir).getCodigo()).getAsDouble())*cantidadAConvertir);
-
-        switch (opcion){
-            case 1:
-                if (listaCodigos.contains(monedas.get(paisSeleccionadoAConvertir).getCodigo())){
-                    double resultado = conversionRates.get(monedas.get(paisSeleccionadoAConvertir).getCodigo()).getAsDouble();
-                    System.out.println("Actualmente " + cantidadAConvertir+ " "+
-                            monedas.get(paisSeleccionado).getCodigo()+ " Equivalen a: "+
-                            cantidadAConvertir*resultado+" "+monedas.get(paisSeleccionadoAConvertir).getCodigo());
+            if(opcion == contador[0]){
+                continuar = false;
+                System.out.println("Saliendo del conversor de moneda, Hasta pronto!");
                 break;
+            }
+
+            System.out.println("-----------------------------------------------");
+            System.out.println("Ingresa la cantidad a convertir: "+"\n");
+
+            double cantidadAConvertir = scanner.nextDouble();
+
+            System.out.println("-----------------------------------------------");
+            System.out.println("Selecciona la moneda de conversión"+"\n");
+
+            final int[] contadorConvertidor = {1};
+            monedas.forEach((pais, mon) -> {
+                System.out.println(contadorConvertidor[0] + " "+pais + " "+mon.getCodigo());
+                contadorConvertidor[0]++;
+            });
+
+            int opcionConvertir = scanner.nextInt();
+
+            List<Integer> listaOpciones = new ArrayList<>();
+            listaOpciones.add(opcion);
+            listaOpciones.add(opcionConvertir);
+
+            String paisSeleccionado = lista.get(opcion - 1);
+            String paisSeleccionadoAConvertir = lista.get(opcionConvertir - 1);
+
+            String url = "https://v6.exchangerate-api.com/v6/";
+            String apiKey = "ad70ff102a291a8321ed3820";
+            String complemento = "/latest/"+monedas.get(paisSeleccionado).getCodigo();
+
+
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(url+apiKey+complemento)).build();
+            HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            String json = httpResponse.body();
+
+            Gson gson = new Gson();
+            JsonElement jsonElement = JsonParser.parseString(json);
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+            JsonObject conversionRates = jsonObject.getAsJsonObject("conversion_rates");
+
+            List<String> listaCodigos = new ArrayList<>();
+            conversionRates.keySet().forEach(listaCodigos::add);
+
+
+            for (int i = 0; i < lista.size(); i++) {
+                if (i + 1 == listaOpciones.get(0)){
+                    if (listaCodigos.contains(monedas.get(paisSeleccionadoAConvertir).getCodigo())){
+                        double resultado = conversionRates.get(monedas.get(paisSeleccionadoAConvertir).getCodigo()).getAsDouble();
+                        System.out.println("Actualmente " + cantidadAConvertir+ " "+
+                                monedas.get(paisSeleccionado).getCodigo()+ " Equivalen a: "+
+                                cantidadAConvertir*resultado+" "+monedas.get(paisSeleccionadoAConvertir).getCodigo());
+                    }
                 }
+            }
 
+            System.out.println("-----------------------------------------------");
+            System.out.println("¿Deseas realizar otra conversión? (1 = Sí, 2 = No)");
+            int continuarOpcion = scanner.nextInt();
+
+            if (continuarOpcion != 1) {
+                continuar = false;
+                System.out.println("Gracias por usar el conversor de moneda. ¡Hasta luego!");
+            }
         }
-
-
-
     }
 }
